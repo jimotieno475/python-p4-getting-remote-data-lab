7abc38ba06c169097013da72cb56af9660bf54b6
jim <jim.otieno@student.moringaschool.com>
2024-01-10 23:18:33 +0300

    completed

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,23 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
+        url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+        response = requests.get(url)
+        return response.content
         pass
 
     def load_json(self):
+
+        programs_list = []
+        programs = json.loads(self.get_response_body())
+        for program in programs:
+            programs_list.append(program["agency"])
+
+        return programs_list  
+
         pass
+programs = GetRequester()
+programs_schools = programs.load_json()
+
+for school in set(programs_schools):
+    print(school)
